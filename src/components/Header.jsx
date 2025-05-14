8b30f2f9cb8f308bd06f0801d955af39fb8d6f22
mtrs247h <hiroki880501@gmail.com>
2025-05-15 05:43:07 +0900

    インデント修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
-    return (
-        <header className='w-full h-16 bg-black flex justify-between items-center px-4'>
-            <Link to="/" className='text-white mr-4'>
-            Blog
-            </Link>
-            <Link to="/contact" className='text-white'>
-            お問い合わせ
-            </Link>
-        </header>
-    )
+  return (
+    <header className='w-full h-16 bg-black flex justify-between items-center px-4'>
+      <Link to="/" className='text-white mr-4'>
+        Blog
+      </Link>
+      <Link to="/contact" className='text-white'>
+        お問い合わせ
+      </Link>
+    </header>
+  )
 };
